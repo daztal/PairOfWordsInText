92f7048619470df3151437c34c0cd3ab43e39ea7
daztal <daztal@mail.ru>
2017-06-14 01:20:00 +0300

    Add project files.

---
+++ PairOfWordsInText/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairOfWordsInText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string absolutePath = @"C:\Users\Viachaslau\Documents\visual studio 2015\Projects\PairOfWordsInText\PairOfWordsInText\TextFile1.txt";
+            string relativePath = @"TextFile1.txt";
+            string text = File.ReadAllText(absolutePath);
+            WordPairsCounter FinalCounter = new WordPairsCounter();
+            var result = FinalCounter.SelectMostFrequentPairs(text);
+			int counter = 0;
+			foreach (var pair in result.Reverse())
+			{
+				if (counter == 20)
+				{
+					break;
+				}
+                Console.WriteLine(pair.Key + ": " + pair.Value);
+				counter++;
+            }
+        }
+    }
+}
---
+++ PairOfWordsInText/WordPairsCounter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairOfWordsInText
+{
+    class WordPairsCounter
+    {
+        public Dictionary<string, int> SelectMostFrequentPairs(string text)
+        {
+            text = text.ToLower();
+			int counter;
+            string[] wordsArr = text.Split();
+			Dictionary<string, int> insideDictionary = new Dictionary<string, int>();
+			for (int i = 0; i < wordsArr.Length - 1; i++)
+			{
+				if (wordsArr[i].EndsWith(",") || wordsArr[i].EndsWith(".") || wordsArr[i].EndsWith("!") || wordsArr[i].EndsWith("?") || wordsArr[i] ==  ("") || wordsArr[i + 1] == ("") || wordsArr[i + 1].StartsWith(",") || wordsArr[i + 1].StartsWith(".") || wordsArr[i + 1].StartsWith("!") || wordsArr[i + 1].StartsWith("?"))
+				{
+					continue;
+				}
+				else
+				{
+					string first = wordsArr[i].Trim(new char[] { ',', '.', '!', '?', '\'', '\"' });
+					string second = wordsArr[i + 1].Trim(new char[] { ',', '.', '!', '?', '\'', '\"' });
+					if (insideDictionary.ContainsKey(first + " " + second))
+					{
+						if (insideDictionary.TryGetValue(first + " " + second, out counter))
+						{
+							insideDictionary[first + " " + second] = insideDictionary[first + " " + second] + 1;
+						}
+					}
+					else
+					{
+						insideDictionary.Add(first + " " + second, 1);
+					}
+				}
+			}
+			var newDictionary = insideDictionary.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+			return newDictionary;
+        }
+    }
+}
